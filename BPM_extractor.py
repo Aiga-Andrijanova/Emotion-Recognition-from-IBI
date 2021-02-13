c783631b73237950bd4a526677b330f087bc78c4
Aiga Andrijanova <aigaandrijanova@gmail.com>
2021-02-13 15:21:08 +0200

    Added aditional args

--- BPM_extractor.py
+++ BPM_extractor.py
@@ -8,7 +8,9 @@ from tqdm import tqdm
 import argparse
 
 parser = argparse.ArgumentParser(add_help=False)
-parser.add_argument('-PATH', default=f'D:/Universitātes darbi/Bakalaura darbs/Datasets/AMIGOS/Data_Test/', type=str)
+parser.add_argument('-DATA_PATH', default=f'D:/Universitātes darbi/Bakalaura darbs/Datasets/AMIGOS/Data_Preprocessed/', type=str)
+parser.add_argument('-JSON_PATH', default=f'Data/AllBPMdata.json', type=str)
+parser.add_argument('-MEMMAP_PATH', default=f'Data/AllBPMdata.memmap', type=str)
 parser.add_argument('-FREQ', default=128, type=int)
 parser.add_argument('-SLIDING_WINDOW', default=10, type=int)
 parser.add_argument('-TIMESTEP', default=1, type=int)
@@ -19,18 +21,18 @@ valence_list = []
 arousal_list = []
 lenghts = []
 
-memmap_file = np.memmap('Data/BPMdata.memmap', dtype='float16', mode='w+', shape=(1,))
+memmap_file = np.memmap(args.MEMMAP_PATH, dtype='float16', mode='w+', shape=(1,))
 memmap_file.flush()
 memmap_idx = 0
 
 person = 1
 failed = 0
-for filename in os.listdir(args.PATH):
+for filename in os.listdir(args.DATA_PATH):
 
-    DATA = loadmat(args.PATH + filename)
+    DATA = loadmat(args.DATA_PATH + filename)
 
     # There are 20 videos, 0-15 short videos, 16-19 long
-    for video in tqdm(range(0, 1)):
+    for video in tqdm(range(0, 15)):
 
         arousal = DATA['labels_selfassessment'][0][video][0, 0]
         valence = DATA['labels_selfassessment'][0][video][0, 1]
@@ -40,9 +42,10 @@ for filename in os.listdir(args.PATH):
 
         if np.isnan(np.max(LeadIII)):
             failed = failed + 1
-            break
+            continue
 
-        ts, filtered, rpeaks, templates_ts, templates, heart_rate_ts, heart_rate = ecg.ecg(signal=LeadIII, sampling_rate=args.FREQ, show=False)
+        ts, filtered, rpeaks, templates_ts, templates, heart_rate_ts, heart_rate = \
+            ecg.ecg(signal=LeadIII, sampling_rate=args.FREQ, show=False)
 
         window_count = int((int(np.max(ts)) - args.SLIDING_WINDOW + args.TIMESTEP) / args.TIMESTEP)
 
@@ -57,7 +60,8 @@ for filename in os.listdir(args.PATH):
                 else:
                     break
 
-            memmap_file = np.memmap('Data/BPMdata.memmap', dtype='float16', mode='r+', shape=(len(hr),), offset=2 * memmap_idx)
+            memmap_file = np.memmap(args.MEMMAP_PATH, dtype='float16',
+                                    mode='r+', shape=(len(hr),), offset=2 * memmap_idx)
 
             j = 0
             for val in hr:
@@ -85,7 +89,7 @@ json_dict["arousal"] = arousal_list
 json_dict["valence"] = valence_list
 json_dict["lenghts"] = lenghts
 
-with open('Data/BPMdata.json', 'w+') as json_file:
+with open(args.JSON_PATH, 'w+') as json_file:
     json.dump(json_dict, json_file, indent=4)
 
 print(f'{failed} videos have nan in ecg readings')
