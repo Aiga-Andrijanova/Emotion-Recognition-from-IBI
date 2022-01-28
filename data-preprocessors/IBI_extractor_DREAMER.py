8d8454b9c2d2af74350d145dd81ef943b9305a30
Aiga Andrijanova <aigaandrijanova@gmail.com>
2022-01-28 12:28:32 +0200

    DREAMER preprocessor updates and standart score norm added

--- data-preprocessors/IBI_extractor_DREAMER.py
+++ data-preprocessors/IBI_extractor_DREAMER.py
@@ -8,13 +8,15 @@ import argparse
 from modules.dataset_utils import DatasetUtils
 
 parser = argparse.ArgumentParser(add_help=False)
-parser.add_argument('-DATA_PATH', default=f'D:/Universitātes darbi/Bakalaura darbs/Datasets/DREAMER/data/', type=str)
-parser.add_argument('-JSON_PATH', default=f'../data/DREAMER_IBI_10sec_byseq.json', type=str)
-parser.add_argument('-MEMMAP_PATH', default=f'../data/DREAMER_IBI_10sec_byseq.mmap', type=str)
-parser.add_argument('-FREQ', default=256, type=int)
-parser.add_argument('-SLIDING_WINDOW', default=10, type=int)
+parser.add_argument('-DATA_PATH', default=f'../datasets/DREAMER/data/', type=str)
+parser.add_argument('-OUTPUT_PATH', default=f'../data_processed_full/', type=str)
+parser.add_argument('-FREQ', default=256, type=int)  # Sampling freq of the dataset, don't change this
+parser.add_argument('-CLASS_COUNT', default=2, type=int)  # [2]
+parser.add_argument('-SLIDING_WINDOW', default=30, type=int)
 parser.add_argument('-TIMESTEP', default=1, type=int)
-parser.add_argument('-MAX_SEQ_LEN', default=19, type=int)  # 55 for 30 sec / 38 for 20 sec / 19 for 10 sec
+parser.add_argument('-MAX_SEQ_LEN', default=55, type=int)  # 55 for 30 sec / 38 for 20 sec / 19 for 10 sec
+parser.add_argument('-NORMALIZATION_TYPE', default='minmax', type=str)  # [minmax; standard_score]
+parser.add_argument('-NORMALIZATION_SCOPE', default='byperson', type=str)  # [byperson; byseq]
 args, other_args = parser.parse_known_args()
 
 person_id = []
@@ -22,6 +24,9 @@ valence_list = []
 arousal_list = []
 lengths = []
 
+mmap_path = args.OUTPUT_PATH \
+            + f'DREAMER_IBI_{args.SLIDING_WINDOW}sec_{args.CLASS_COUNT}C_' \
+              f'{args.NORMALIZATION_TYPE}_{args.NORMALIZATION_SCOPE}.mmap'
 memmap_file = np.memmap(args.MEMMAP_PATH, dtype='float16', mode='w+', shape=(1,))
 memmap_file.flush()
 memmap_idx = 0
@@ -29,14 +34,16 @@ memmap_idx = 0
 person = 1
 failed = 0
 
+# Examples how to access data:
 # DATA['DREAMER']['Data'][0,0][0,11]['ScoreValence'][0][0][17][0] 11. dalībnieka 17. video valence
 # DATA['DREAMER']['Data'][0,0][0,11]['ECG'][0,0]['stimuli'][0,0][17,0][:, 0] 11. dalībnieka 17. video 1. ECG lead
 
 dataset = loadmat(args.DATA_PATH + 'DREAMER.mat')
 data = dataset['DREAMER']['Data'][0, 0]
-del(dataset)
+del dataset
 
 for participant in range(0, 23):  # 23 participants
+    print(f'Starting to process {participant}. participant \n')
     for video in tqdm(range(0, 18)):  # 18 video clips
 
         arousal = data[0, participant]['ScoreArousal'][0][0][video][0]
@@ -52,10 +59,11 @@ for participant in range(0, 23):  # 23 participants
             valence = 0
 
         LeadII = data[0, participant]['ECG'][0, 0]['stimuli'][0, 0][video, 0][:, 0]
-        # LeadI also is available
+        # LeadI is also available
 
         if np.isnan(np.max(LeadII)):
             failed = failed + 1
+            print(f'LeadII data contains nan in {participant}. participants, {video}. video')
             continue
 
         try:
@@ -77,7 +85,8 @@ for participant in range(0, 23):  # 23 participants
             T = (length - 1) / args.FREQ
             ts = np.linspace(0, T, length, endpoint=False)
         except:
-            pass
+            print(f'Failed to filter signal for {participant}. participants {video}. video')
+            failed = failed + 1
 
         window_count = int((int(np.max(ts)) - args.SLIDING_WINDOW + args.TIMESTEP) / args.TIMESTEP)
 
@@ -91,11 +100,6 @@ for participant in range(0, 23):  # 23 participants
                     ibi.append(ts[rpeaks][idx + 1] - ts[rpeaks][idx])
                 else:
                     break
-
-            # if len(ibi) < 20:
-            #     ibi.clear()
-            #     window_start_time = window_start_time + args.TIMESTEP
-            #     continue
 
             memmap_file = np.memmap(args.MEMMAP_PATH, dtype='float16',
                                     mode='r+', shape=(args.MAX_SEQ_LEN,), offset=2 * memmap_idx)
@@ -134,10 +138,30 @@ valence_weights, class_count = DatasetUtils.weight_calculation(feature_list=vale
 # WEIGHT CALCULATION
 
 # DATA NORMALIZATION
-DatasetUtils.normalization_minmax_by_sample(
-    memmap_path=args.MEMMAP_PATH,
-    shape=tuple([len(lengths), args.MAX_SEQ_LEN])
-)
+if args.NORMALIZATION_TYPE == 'minmax':
+    if args.NORMALIZATION_SCOPE == 'byperson':
+        DatasetUtils.normalization_minmax_by_person(
+            memmap_path=mmap_path,
+            shape=tuple([len(lengths), args.MAX_SEQ_LEN]),
+            person_ids=person_id
+        )
+    elif args.NORMALIZATION_SCOPE == 'byseq':
+        DatasetUtils.normalization_minmax_by_sample(
+            memmap_path=mmap_path,
+            shape=tuple([len(lengths), args.MAX_SEQ_LEN])
+        )
+elif args.NORMALIZATION_TYPE == 'standard_score':
+    if args.NORMALIZATION_SCOPE == 'byperson':
+        DatasetUtils.normalization_standard_score_by_person(
+            memmap_path=mmap_path,
+            shape=tuple([len(lengths), args.MAX_SEQ_LEN]),
+            person_ids=person_id
+        )
+    elif args.NORMALIZATION_SCOPE == 'byseq':
+        DatasetUtils.normalization_standard_score_by_sample(
+            memmap_path=mmap_path,
+            shape=tuple([len(lengths), args.MAX_SEQ_LEN])
+        )
 # DATA NORMALIZATION
 
 json_dict = dict()
@@ -150,7 +174,8 @@ json_dict["lengths"] = lengths
 json_dict["valence_weights"] = valence_weights
 json_dict["arousal_weights"] = arousal_weights
 
-with open(args.JSON_PATH, 'w+') as json_file:
+json_path = mmap_path.replace('.mmap', '.json')
+with open(json_path, 'w+') as json_file:
     json.dump(json_dict, json_file, indent=4)
 
 print(f'{failed} videos have nan in ecg readings')
--- modules/dataset_utils.py
+++ modules/dataset_utils.py
@@ -28,20 +28,16 @@ class DatasetUtils():
 
         for i in range(len(memmap_file)-1):
             data_max = np.max(memmap_file[i])
-            data_min = np.min(memmap_file[i, np.nonzero(memmap_file[i])])
+            data_min = np.min(memmap_file[i])
 
             delta = data_max - data_min
-            # for j in range(max_seq_len):
-            #     if memmap_file[i, j] != 0:
-            #         memmap_file[i, j] = (memmap_file[i, j] - data_min) / (delta + eps) \
-            #                             * (range_max - range_min) + range_min
             memmap_file[i, :max_seq_len] = (memmap_file[i, :max_seq_len] - data_min) / (delta + eps) \
                                            * (range_max - range_min) + range_min
             memmap_file.flush()
 
     @staticmethod
     def normalization_minmax_by_person(memmap_path, shape, person_ids, range_min=0, range_max=1.0):
-        # Normalizes data by each person, assumes that data is ordered by person
+        # Normalizes data by each person, assumes that memmap data is ordered by person
 
         memmap_file = np.memmap(memmap_path, dtype='float16',
                                 mode='r+', shape=shape)
@@ -66,4 +62,46 @@ class DatasetUtils():
 
             memmap_file[start_idx:end_idx, :max_seq_len] = (memmap_file[start_idx:end_idx, :max_seq_len] - data_min) / \
                                                            (delta + eps) * (range_max - range_min) + range_min
+            memmap_file.flush()
+
+    @staticmethod
+    def normalization_standard_score_by_sample(memmap_path, shape):
+        memmap_file = np.memmap(memmap_path, dtype='float16',
+                                mode='r+', shape=shape)
+        max_seq_len = memmap_file.shape[1]
+        eps = 1e-8
+
+        for i in range(len(memmap_file)-1):
+            data_mean = np.mean(memmap_file[i])
+            data_std = np.std(memmap_file[i])
+
+            memmap_file[i, :max_seq_len] = (memmap_file[i, :max_seq_len] - data_mean) / (data_std + eps)
+            memmap_file.flush()
+
+    @staticmethod
+    def normalization_standard_score_by_person(memmap_path, shape, person_ids):
+        # Normalizes data by each person, assumes that memmap data is ordered by person
+
+        memmap_file = np.memmap(memmap_path, dtype='float16',
+                                mode='r+', shape=shape)
+        max_seq_len = memmap_file.shape[1]
+        eps = 1e-8
+
+        person_ids.reverse()
+        person_count = person_ids[0]
+        person_change_idx = []
+        for person in range(1,person_count):
+            person_change_idx.append(person_ids.index(person))
+        person_change_idx.append(0)
+        person_change_idx.reverse()
+
+        for person in range(1, len(person_change_idx)-1):
+            start_idx = person_change_idx[person-1]
+            end_idx = person_change_idx[person]
+
+            data_mean = np.mean(memmap_file[start_idx:end_idx])
+            data_std = np.std(memmap_file[start_idx:end_idx])
+
+            memmap_file[start_idx:end_idx, :max_seq_len] = (memmap_file[start_idx:end_idx, :max_seq_len] - data_mean) / \
+                                                           (data_std + eps)
             memmap_file.flush()
